c9927908cc8f693e2faa7dcc1fd0187e8959db2b
Jonathan Serafini <jonathan@lightspeedretail.com>
2014-10-16 13:10:34 -0400

    Version bump to 10.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "jonathan.serafini@lightspeedretail.com"
 license          "Apache 2.0"
 description      "LWRPs for managing SSH assets, inspired by github.com/markolson/chef-ssh"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
